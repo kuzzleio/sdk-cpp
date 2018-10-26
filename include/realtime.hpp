3ee5f6b655a077fe8f80a640d68d2aaaea8fb8bc
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-10-26 16:06:25 +0200

    Remove realtime deprecated methods

--- include/realtime.hpp
+++ include/realtime.hpp
@@ -22,11 +22,9 @@ namespace kuzzleio {
       Realtime(Kuzzle* kuzzle, realtime* realtime);
       virtual ~Realtime();
       int count(const std::string& index, const std::string& collection, const std::string& roomId, query_options *options=nullptr);
-      std::string list(const std::string& index, const std::string& collection, query_options *options=nullptr);
       void publish(const std::string& index, const std::string& collection, const std::string& body, query_options *options=nullptr);
       std::string subscribe(const std::string& index, const std::string& collection, const std::string& body, NotificationListener* cb, room_options* options=nullptr);
       void unsubscribe(const std::string& roomId, query_options *options=nullptr);
-      bool validate(const std::string& index, const std::string& collection, const std::string& body, query_options *options=nullptr);
 
       NotificationListener* getListener(const std::string& roomId);
   };
