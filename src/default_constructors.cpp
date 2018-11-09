ae030ffd51c2c35e07d2eb2baed1378c9c397755
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-09 16:35:46 +0100

    [default_constructors.cpp] include core.hpp

--- src/default_constructors.cpp
+++ src/default_constructors.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "kuzzle.h"
+#include "core.hpp"
 
 namespace kuzzleio {
   s_options::s_options() {
