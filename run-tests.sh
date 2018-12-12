f7219b8f597c6e2d29f59fede145638d9963eb95
Sébastien Cottinet <seb.cottinet@gmail.com>
2018-12-12 14:25:20 +0100

    Add valgrind to functional tests (#38)
    :warning: depends on https://github.com/kuzzleio/sdk-cross/pull/1 to make tests pass (we need the valgrind package in the docker test images) :warning: 
    
    # Description
    
    Add [valgrind](http://valgrind.org/) on top of functional tests, automatically generating a report available at the following path: `test/_build_cpp_tests/valgrind_report.log`
    
    Do not review any of the `*.supp` files: those are only rules asking valgrind to suppress detected issues. The aim of these files, at least for now, is only to suppress Go and CGo errors (see below).
    
    # Go and CGo errors
    
    Go and CGo issues detected by valgrind are irrelevant. If needed, dedicated profiling should be performed directly on those layers instead.
    
    The valgrind report should hide most, if not all, Go and Cgo errors, thanks to the `valgrind.*.supp` suppression files stored in the `test/` directory.
    
    If new errors appear from these 2 layers, then the suppression files should be amended accordingly, by using the suppression snippet provided in the valgrind report (more information in the [valgrind documentation](http://valgrind.org/docs/manual/manual-core.html#manual-core.suppress))
    
    :warning: be wary when adding new suppression rules: not all warnings with a Go file in the stacktrace are to be suppressed. For instance, a memory leak report because of a calloc made in CGo will partly show Go files in the stacktrace, but the leak is real, in the C++ world, and must be fixed
    
    # Valgrind report impact on the CI
    
    For now, there are so many problems to be fixed that the valgrind report is informational only, and does not block the CI.
    Having the report automatically generated will help us track the bugs to be fixed. Once all valgrind issues are solved, we'll have to parse the log file and block the CI if new issues are detected.
    
    # Other changes
    
    - The C SDK shared library is now used for cucumber, instead of the static one. This allows us to efficiently suppress most of the false positives from the CGo world. 
    - ~A global "After" hook has been added to the cucumber test suite, executed after each scenario. This hook frees the instances of Kuzzle and Protocol in the global context, preventing meaningless memleak reports from valgrind~ (reverted, this causes a crash of currently unknown source with tests running on ARM 32 bits architecture)
    - The "run_test" rule in the makefile now runs the `run-tests.sh` script, instead of having its own way of starting tests. This DRYifies the process and makes testing more consistent

--- run-tests.sh
+++ run-tests.sh
@@ -1,17 +1,65 @@
 #!/usr/bin/env bash
 
-set -e
-
 cd test
+
+export CUCUMBER_HOST='localhost'
+export CUCUMBER_PORT='3902'
+
+VALGRIND_LOGFILE='./_build_cpp_tests/valgrind_report.log'
+
+# initializes the valgrind suppression arguments
+for supp in ./valgrind.*.supp; do
+  VALGRIND_SUPPR="${VALGRIND_SUPPR} --suppressions=${supp}"
+done
 
 FEATURE_FILE=$1
 
-./build_cpp_tests.sh
-./_build_cpp_tests/KuzzleSDKStepDefs &
+if [ -z "${SKIPBUILD}" -o "${SKIPBUILD}" = 0 ]; then
+  ./build_cpp_tests.sh
+fi
+
+if [ -z "${SKIPVALGRIND}" -o "${SKIPVALGRIND}" = 0 ]; then
+  valgrind --leak-check=full --show-reachable=yes --gen-suppressions=all ${VALGRIND_SUPPR} --log-file=${VALGRIND_LOGFILE} ./_build_cpp_tests/KuzzleSDKStepDefs &
+
+  # Should generally be enough to let valgrind initialize
+  # and start the server.
+  # If not, there is a retry mechanism fallback below
+  sleep 2
+else
+  ./_build_cpp_tests/KuzzleSDKStepDefs &
+fi
+
+CMD="cucumber"
 
 if [ ! -z "$FEATURE_FILE" ]; then
-  bundle exec cucumber ./features/sdk-features/$FEATURE_FILE
-else
-  bundle exec cucumber
+  CMD="${CMD} ./features/sdk-features/$FEATURE_FILE"
 fi
- cd -
+
+${CMD}
+CUCUMBER_STATUS=$?
+
+# Restart Cucumber if the wire server wasn't ready
+# We cannot check the connection port beforehand (e.g. with netcat),
+# because the wire protocol makes the step definitions exit whenever
+# a disconnection is detected
+retries=5
+
+while [ ${CUCUMBER_STATUS} -eq 2 ]; do
+  if [ ${retries} -eq 0 ]; then
+    echo "No more retries available. Aborting."
+    exit 2
+  fi
+
+  retries=$((retries-1))
+
+  echo "Cucumber Wire connection problem detected."
+  echo "Retrying in 5 seconds (${retries} retries left)."
+  sleep 5
+
+  ${CMD}
+  CUCUMBER_STATUS=$?
+done
+
+cd -
+
+exit ${CUCUMBER_STATUS}
