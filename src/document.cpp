041af0c75db30664cb52b6f301f150b060a73218
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-12-23 21:27:09 +0100

    [multi-sig] document controller ok

--- src/document.cpp
+++ src/document.cpp
@@ -30,113 +30,299 @@ namespace kuzzleio {
     free(_document);
   }
 
-  int Document::count(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(int_result, r, kuzzle_document_count(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+  int Document::count(const std::string& index, const std::string& collection) {
+    query_options options;
 
-    int ret = r->result;
-    kuzzle_free_int_result(r);
-    return ret;
+    return this->count(index, collection, options);
   }
 
-  int Document::count(const std::string& index, const std::string& collection, query_options *options) {
+  int Document::count(const std::string& index, const std::string& collection, const query_options& options) {
     return this->count(index, collection, "{}", options);
   }
 
-  bool Document::exists(const std::string& index, const std::string& collection, const std::string& id, query_options *options) {
-    KUZZLE_API(bool_result, r, kuzzle_document_exists(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), options))
+  int Document::count(const std::string& index, const std::string& collection, const std::string& query) {
+    query_options options;
+
+    return this->count(index, collection, options);
+  }
+
+  int Document::count(const std::string& index, const std::string& collection, const std::string& query, const query_options& options) {
+    KUZZLE_API(int_result, r, kuzzle_document_count(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(query.c_str()),
+      const_cast<query_options*>(&options)))
+
+    int ret = r->result;
+    kuzzle_free_int_result(r);
+
+    return ret;
+  }
+
+
+  bool Document::exists(const std::string& index, const std::string& collection, const std::string& id) {
+    query_options options;
+
+    return this->exists(index, collection, id, options);
+  }
+
+  bool Document::exists(const std::string& index, const std::string& collection, const std::string& id, const query_options& options) {
+    KUZZLE_API(bool_result, r, kuzzle_document_exists(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<query_options*>(&options)))
 
     bool ret = r->result;
     kuzzle_free_bool_result(r);
+
     return ret;
   }
 
-  std::string Document::create(const std::string& index, const std::string& collection, const std::string& id, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_create(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::create(const std::string& index, const std::string& collection, const std::string& id, const std::string& document) {
+    query_options options;
+
+    return this->create(index, collection, id, document, options);
+  }
+
+  std::string Document::create(const std::string& index, const std::string& collection, const std::string& id, const std::string& document, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_create(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<char*>(document.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::createOrReplace(const std::string& index, const std::string& collection, const std::string& id, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_create_or_replace(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::createOrReplace(const std::string& index, const std::string& collection, const std::string& id, const std::string& document) {
+    query_options options;
+
+    return this->createOrReplace(index, collection, id, document, options);
+  }
+
+  std::string Document::createOrReplace(const std::string& index, const std::string& collection, const std::string& id, const std::string& document, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_create_or_replace(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<char*>(document.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::delete_(const std::string& index, const std::string& collection, const std::string& id, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_delete(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), options))
+
+  std::string Document::delete_(const std::string& index, const std::string& collection, const std::string& id) {
+    query_options options;
+
+    return this->delete_(index, collection, id, options);
+  }
+
+  std::string Document::delete_(const std::string& index, const std::string& collection, const std::string& id, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_delete(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::vector<std::string> Document::deleteByQuery(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(string_array_result, r, kuzzle_document_delete_by_query(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::vector<std::string> Document::deleteByQuery(const std::string& index, const std::string& collection, const std::string& query) {
+    query_options options;
+
+    return this->deleteByQuery(index, collection, query, options);
+  }
+
+  std::vector<std::string> Document::deleteByQuery(const std::string& index, const std::string& collection, const std::string& query, const query_options& options) {
+    KUZZLE_API(string_array_result, r, kuzzle_document_delete_by_query(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(query.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::vector<std::string> v = std::vector<std::string>(r->result, r->result + r->result_length);
     kuzzle_free_string_array_result(r);
+
     return v;
   }
 
-  std::string Document::get(const std::string& index, const std::string& collection, const std::string& id, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_get(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), options))
+
+  std::string Document::get(const std::string& index, const std::string& collection, const std::string& id) {
+    query_options options;
+
+    return this->get(index, collection, id, options);
+  }
+
+  std::string Document::get(const std::string& index, const std::string& collection, const std::string& id, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_get(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::replace(const std::string& index, const std::string& collection, const std::string& id, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_replace(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::replace(const std::string& index, const std::string& collection, const std::string& id, const std::string& document) {
+    query_options options;
+
+    return this->replace(index, collection, id, document, options);
+  }
+
+  std::string Document::replace(const std::string& index, const std::string& collection, const std::string& id, const std::string& document, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_replace(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<char*>(document.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::update(const std::string& index, const std::string& collection, const std::string& id, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_update(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(id.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::update(const std::string& index, const std::string& collection, const std::string& id, const std::string& document) {
+    query_options options;
+
+    return this->update(index, collection, id, document, options);
+  }
+
+  std::string Document::update(const std::string& index, const std::string& collection, const std::string& id, const std::string& document, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_update(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(id.c_str()),
+      const_cast<char*>(document.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  bool Document::validate(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(bool_result, r, kuzzle_document_validate(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  bool Document::validate(const std::string& index, const std::string& collection, const std::string& document) {
+    query_options options;
+
+    return this->validate(index, collection, document, options);
+  }
+
+  bool Document::validate(const std::string& index, const std::string& collection, const std::string& document, const query_options& options) {
+    KUZZLE_API(bool_result, r, kuzzle_document_validate(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(document.c_str()),
+      const_cast<query_options*>(&options)))
 
     bool ret = r->result;
     kuzzle_free_bool_result(r);
+
     return ret;
   }
 
-  SearchResult* Document::search(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(search_result, r, kuzzle_document_search(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  SearchResult* Document::search(const std::string& index, const std::string& collection, const std::string& query) {
+    query_options options;
+
+    return this->search(index, collection, query, options);
+  }
+
+  SearchResult* Document::search(const std::string& index, const std::string& collection, const std::string& query, const query_options& options) {
+    KUZZLE_API(search_result, r, kuzzle_document_search(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(query.c_str()),
+      const_cast<query_options*>(&options)))
+
     return new SearchResult(r);
   }
 
-  std::string Document::mCreate(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_mcreate(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::mCreate(const std::string& index, const std::string& collection, const std::string& documents) {
+    query_options options;
+
+    return this->mCreate(index, collection, documents, options);
+  }
+
+  std::string Document::mCreate(const std::string& index, const std::string& collection, const std::string& documents, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_mcreate(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(documents.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::mCreateOrReplace(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_mcreate_or_replace(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::mCreateOrReplace(const std::string& index, const std::string& collection, const std::string& documents) {
+    query_options options;
+
+    return this->mCreateOrReplace(index, collection, documents, options);
+  }
+
+  std::string Document::mCreateOrReplace(const std::string& index, const std::string& collection, const std::string& documents, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_mcreate_or_replace(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(documents.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::vector<std::string> Document::mDelete(const std::string& index, const std::string& collection, const std::vector<std::string>& ids, query_options *options) {
+
+  std::vector<std::string> Document::mDelete(const std::string& index, const std::string& collection, const std::vector<std::string>& ids) {
+    query_options options;
+
+    return this->mDelete(index, collection, ids, options);
+  }
+
+  std::vector<std::string> Document::mDelete(const std::string& index, const std::string& collection, const std::vector<std::string>& ids, const query_options& options) {
     char **idsArray = new char *[ids.size()];
+
     for (size_t i = 0; i < ids.size(); i++) {
       idsArray[i] = const_cast<char*>(ids[i].c_str());
     }
@@ -144,16 +330,30 @@ namespace kuzzleio {
     KUZZLE_API(
       string_array_result,
       r,
-      kuzzle_document_mdelete(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), idsArray, ids.size(), options),
+      kuzzle_document_mdelete(_document,
+        const_cast<char*>(index.c_str()),
+        const_cast<char*>(collection.c_str()),
+        idsArray,
+        ids.size(),
+        const_cast<query_options*>(&options)),
       delete[] idsArray)
 
     std::vector<std::string> v = std::vector<std::string>(r->result, r->result + r->result_length);
     kuzzle_free_string_array_result(r);
+
     return v;
   }
 
-  std::string Document::mGet(const std::string& index, const std::string& collection, const std::vector<std::string>& ids, query_options *options) {
+
+  std::string Document::mGet(const std::string& index, const std::string& collection, const std::vector<std::string>& ids) {
+    query_options options;
+
+    return this->mGet(index, collection, ids, options);
+  }
+
+  std::string Document::mGet(const std::string& index, const std::string& collection, const std::vector<std::string>& ids, const query_options& options) {
     char **idsArray = new char *[ids.size()];
+
     for (size_t i = 0; i < ids.size(); i++) {
       idsArray[i] = const_cast<char*>(ids[i].c_str());
     }
@@ -161,27 +361,60 @@ namespace kuzzleio {
     KUZZLE_API(
       string_result,
       r,
-      kuzzle_document_mget(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), idsArray, ids.size(), options),
+      kuzzle_document_mget(
+        _document,
+        const_cast<char*>(index.c_str()),
+        const_cast<char*>(collection.c_str()),
+        idsArray,
+        ids.size(),
+        const_cast<query_options*>(&options)),
       delete[] idsArray)
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::mReplace(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_mreplace(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::mReplace(const std::string& index, const std::string& collection, const std::string& documents) {
+    query_options options;
+
+    return this->mReplace(index, collection, documents, options);
+  }
+
+  std::string Document::mReplace(const std::string& index, const std::string& collection, const std::string& documents, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_mreplace(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(documents.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 
-  std::string Document::mUpdate(const std::string& index, const std::string& collection, const std::string& body, query_options *options) {
-    KUZZLE_API(string_result, r, kuzzle_document_mupdate(_document, const_cast<char*>(index.c_str()), const_cast<char*>(collection.c_str()), const_cast<char*>(body.c_str()), options))
+
+  std::string Document::mUpdate(const std::string& index, const std::string& collection, const std::string& documents) {
+    query_options options;
+
+    return this->mUpdate(index, collection, documents, options);
+  }
+
+  std::string Document::mUpdate(const std::string& index, const std::string& collection, const std::string& documents, const query_options& options) {
+    KUZZLE_API(string_result, r, kuzzle_document_mupdate(
+      _document,
+      const_cast<char*>(index.c_str()),
+      const_cast<char*>(collection.c_str()),
+      const_cast<char*>(documents.c_str()),
+      const_cast<query_options*>(&options)))
 
     std::string ret = r->result;
     kuzzle_free_string_result(r);
+
     return ret;
   }
 }
